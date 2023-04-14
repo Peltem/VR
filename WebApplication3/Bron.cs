4faa8cc2fef345fb96ef858e5cf6fa427b50842a
Peltem <igor200462283@gmail.com>
2023-04-14 12:42:07 +0700

    Добавьте файлы проекта.

---
+++ WebApplication3/Bron.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3
+{
+    public class Bron
+    {
+        public int ID { get; set; }
+        public string? FIO { get {  } set { value = FIO; } }
+        public int Number { get; set; }
+        public DateOnly date { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ WebApplication3/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3
+{
+    public class User  
+    {
+        public int Id { get; set; }
+        public string? FIO  { get; set; }
+        public int Number { get; set; }
+        public string Pass { get; set; }
+        public string? mail { get; set; }
+    }
+}
