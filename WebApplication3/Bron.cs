e29a58f66ffb9504a05662ba4db25c5bae07b975
Peltem <igor200462283@gmail.com>
2023-04-16 17:16:56 +0700

    Начало сайтика

--- WebApplication3/Bron.cs
+++ WebApplication3/Bron.cs
@@ -3,7 +3,7 @@
     public class Bron
     {
         public int ID { get; set; }
-        public string? FIO { get {  } set { value = FIO; } }
+        //public string? FIO { get {  } set { value = FIO; } }
         public int Number { get; set; }
         public DateOnly date { get; set; }
         public DateTime DateTime { get; set; }
